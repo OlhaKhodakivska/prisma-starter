611db090ab73beb2802e36d084cad1c7dc9e4599
OlhaKhodakivska <olhakhodakivska@gmail.com>
2026-09-08 11:59:22 +0200

    Implement GET endpoint for retrieving all customers

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/client.ts";
 
 import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 const PORT = Number(process.env.PORT);
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
 // TODO: mount your customer/product/order routes here, e.g.
 // app.use("/api/customers", customerRoutes);
+
+// GET ALL CUSTOMERS
+app.get("/api/customers", async (req, res, next) => {
+  try {
+    const customers = await prisma.customer.findMany();
+
+    res.status(200).json({
+      success: true,
+      data: customers,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.use((req, res) => {
   res.status(404).json({ success: false, message: "Route not found" });
